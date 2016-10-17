113bf808f86841265b841e00159bf2c0b836fa36
Jakub Fajkowski <jakub fajkowski>
2016-10-17 16:06:17 +0200

    Added file size column to the table view.

--- src/client/FileInfo.java
+++ src/client/FileInfo.java
@@ -1,15 +1,18 @@
 package client;
 
 import java.io.File;
+import java.text.DecimalFormat;
 import java.util.Date;
 
 public class FileInfo {
     private String name;
+    private Long size;
     private String path;
     private Date lastModified;
 
     public FileInfo(File file){
         this.name = file.getName();
+        this.size = file.length();
         this.path = file.getPath();
         this.lastModified = new Date(file.lastModified());
     }
@@ -17,6 +20,17 @@ public class FileInfo {
 
     public String getName() {
         return name;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public static String getSizeText(Long size) {
+        if(size <= 0) return "0";
+        final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
+        int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
+        return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
     }
 
     public String getPath() {
--- src/client/TableController.java
+++ src/client/TableController.java
@@ -1,11 +1,15 @@
 package client;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 
 import java.io.File;
 import java.net.URL;
@@ -15,6 +19,7 @@ import java.util.ResourceBundle;
 public class TableController extends Controller {
     @FXML private TableView<FileInfo> table;
     @FXML private TableColumn<FileInfo, String> name;
+    @FXML private TableColumn<FileInfo, Long> size;
     @FXML private TableColumn<FileInfo, Date> lastModified;
     @FXML private TableColumn<FileInfo, String> path;
 
@@ -24,9 +29,23 @@ public class TableController extends Controller {
     public void initialize(URL location, ResourceBundle resources) {
         super.initialize(location, resources);
 
-        name.setCellValueFactory(new PropertyValueFactory<FileInfo, String>("name"));
-        lastModified.setCellValueFactory(new PropertyValueFactory<FileInfo, Date>("lastModified"));
-        path.setCellValueFactory(new PropertyValueFactory<FileInfo, String>("path"));
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        size.setCellValueFactory(new PropertyValueFactory<>("size"));
+        size.setCellFactory(c -> new TableCell<FileInfo, Long>() {
+
+            @Override
+            protected void updateItem(Long item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(FileInfo.getSizeText(item));
+                }
+            }
+
+        });
+        lastModified.setCellValueFactory(new PropertyValueFactory<>("lastModified"));
+        path.setCellValueFactory(new PropertyValueFactory<>("path"));
 
         table.setItems(records);
     }
